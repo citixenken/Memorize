e4f130c806fbf843a2af182c28cc5e09f3497205
Ken Muyesu <c1t1z3nk3n@gmail.com>
2025-03-30 21:29:14 +0300

    chore: Shape, ViewModifier, Constants

---
+++ CardView.swift
@@ -0,0 +1,55 @@
+//
+//  CardView.swift
+//  Memorize
+//
+//  Created by Ken Muyesu on 30/03/2025.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    typealias Card = MemoryGame<String>.Card
+
+    let card: Card
+    
+    init(_ card: Card) {
+        self.card = card
+    }
+    
+    var body: some View {
+        Pie(endAngle: .degrees(240))
+            .opacity(0.4)
+            .overlay(
+                Text(card.content)
+                    .font(.system(size: 200))
+                    .minimumScaleFactor(0.01)
+                    .multilineTextAlignment(.center)
+                    .aspectRatio(1, contentMode: .fit)
+                    .padding(5)
+            )
+            .padding(5)
+            .cardify(isFaceUp: card.isFaceUp)
+            .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    typealias Card = MemoryGame<String>.Card
+    
+    static var previews: some View {
+        VStack {
+            HStack {
+                CardView(Card(content: "N", id: "test_1"))
+                CardView(Card(isFaceUp: true, content: "Y", id: "test_2"))
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
+            HStack {
+                CardView(Card(isMatched: true, content: "N", id: "test_1"))
+                CardView(Card(isFaceUp: true, isMatched: true, content: "I must not fear.", id: "test_2"))
+            }
+            
+        }
+        .padding()
+        .foregroundColor(.orange)
+    }
+}
--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -11,14 +11,13 @@ struct EmojiMemoryGameView: View {
     @ObservedObject var viewModel: EmojiMemoryGame
     
     private let aspectRatio: CGFloat = 2/3
+    private let spacing: CGFloat = 4
     
     var body: some View {
         VStack {
-            ScrollView {
-                cards
-                    .animation(.easeOut, value: viewModel.cards)
-            }
-            
+            cards
+                .foregroundColor(viewModel.color)
+                .animation(.default, value: viewModel.cards)
             Button("Shuffle") {
                 viewModel.shuffle()
             }
@@ -29,37 +28,10 @@ struct EmojiMemoryGameView: View {
     private var cards: some View {
         AspectVGrid(viewModel.cards, aspectRatio: aspectRatio) { card in
             CardView(card)
-                .padding(4)
+                .padding(spacing)
                 .onTapGesture {
                     viewModel.choose(card)
                 }
-        }
-        // MARK: - height issue here!
-        .frame(height: 700)
-        .foregroundColor(.teal)
-    }
-    struct CardView: View {
-        let card: MemoryGame<String>.Card
-        
-        init(_ card: MemoryGame<String>.Card) {
-            self.card = card
-        }
-        
-        var body: some View {
-            ZStack {
-                let rr = RoundedRectangle(cornerRadius: 12)
-                
-                Group {
-                    rr.strokeBorder(lineWidth: 2)
-                    Text(card.content)
-                        .font(.system(size: 200))
-                        .minimumScaleFactor(0.01)
-                        .aspectRatio(1, contentMode: .fit)
-                }
-                .opacity(card.isFaceUp ? 1 : 0)
-                rr.fill().opacity(card.isFaceUp ? 0 : 1)
-            }
-            .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
         }
     }
 }
